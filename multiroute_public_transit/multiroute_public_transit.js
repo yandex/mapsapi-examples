e31f5279b15cbacbb72e9985b9f8f74f27b54baa
Alexander Chupakhin <sanarise@yandex.ru>
2014-11-18 23:54:23 +0300

    masstransit example refactoring

--- multiroute_public_transit/multiroute_public_transit.js
+++ multiroute_public_transit/multiroute_public_transit.js
@@ -1,58 +1,50 @@
 function init () {
-    var myMap = new ymaps.Map('map', {
-            center: [55.739625, 37.54120],
-            zoom: 12,
-            controls: []
-        }),
-        /**
-         * Создание мультимаршрута.
-         * @param {Object} model Модель мультимаршрута. Задается объектом с полями:
-         * referencePoints - описание опорных точек мультимаршрута (обязательное поле);
-         * params - параметры маршрутизации.
-         * @param {Object} [options] Опции маршрута.
-         * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRoute.xml
-         */
-        multiRoute = new ymaps.multiRouter.MultiRoute({
-            referencePoints: [
-                [55.746908, 37.58328],
-                [55.731461, 37.50707]
-            ]
-        }),
-
-        buttonChangeRoutingMode = new ymaps.control.Button({
-            data: {
-                content: "Переключить на общественный транспорт"
-            },
-            options: {
-                maxWidth: 300
-            }
-        }),
-
-        state = 'auto';
-    buttonChangeRoutingMode.events.add('press', function () {
-        if (state == 'auto') {
-            buttonChangeRoutingMode.data.set('content', 'Переключить на автомобиль');
-            state = 'masstransit';
-            /**
-             * Функция устанавливает параметры маршрутизации.
-             * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRouteModel.xml#setParams
-             */
-            multiRoute.model.setParams({
-                // Тип маршрутизации - на общественном транспорте.
-                routingMode: 'masstransit'
-            });
-        } else if (state == 'masstransit') {
-            buttonChangeRoutingMode.data.set('content', 'Переключить на общественный транспорт');
-            state = 'auto';
-            multiRoute.model.setParams({
-                // Тип маршрутизации - на автомобиле.
-                routingMode: 'auto'
-            });
+    /**
+     * Создаем мультимаршрут.
+     * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRoute.xml
+     */
+    var multiRoute = new ymaps.multiRouter.MultiRoute({
+        referencePoints: [
+            [55.749, 37.524],
+            "Москва, Успенский пер. 7"
+        ],
+        params: {
+            routingMode: 'masstransit'
         }
+    }, {
+        // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
+        boundsAutoApply: true
     });
 
+    // Создаем кнопку.
+    var changeLayoutButton = new ymaps.control.Button({
+        data: { content: "Показывать время для пеших сегментов"},
+        options: { selectOnClick: true }
+    });
+
+    // Объявляем обработчики для кнопки.
+    changeLayoutButton.events.add('select', function () {
+        var contentLayout = ymaps.templateLayoutFactory.createClass('{{ properties.duration.text }}');
+        multiRoute.options.set("routeWalkMarkerIconContentLayout", contentLayout);
+        // Чтобы показывать время для всех сегментов нужно задать
+        // более общую опцию routeMarkerIconContentLayout.
+    });
+
+    changeLayoutButton.events.add('deselect', function () {
+        multiRoute.options.unset("routeWalkMarkerIconContentLayout");
+    });
+
+    // Создаем карту с добавленной на нее кнопкой.
+    var myMap = new ymaps.Map('map', {
+        center: [55.739625, 37.54120],
+        zoom: 12,
+        controls: [changeLayoutButton]
+    }, {
+        buttonMaxWidth: 300
+    });
+
+    // Добавляем мультимаршрут на карту.
     myMap.geoObjects.add(multiRoute);
-    myMap.controls.add(buttonChangeRoutingMode);
 }
 
 ymaps.ready(init);
