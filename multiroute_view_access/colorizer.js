ba37fa4d14be5305fed06bc5c9683e2df03e72b8
Alexander Chupakhin <sanarise@yandex.ru>
2014-11-24 14:37:19 +0300

    комментарии

--- multiroute_view_access/colorizer.js
+++ multiroute_view_access/colorizer.js
@@ -1,26 +1,34 @@
+// Класс, раскрашивающий сегменты маршрута в различные
+// цвета в зависимости от типа сегмента и типа транспорта.
 function Colorizer (multiRoute) {
     this.multiRoute = multiRoute;
 
+    // Подписываемся на события обновления мультимаршрута
+    // и смены активного маршрута.
     multiRoute.events
         .add("update", this.onMultiRouteUpdate, this)
         .add("activeroutechange", this.onActiveRouteChange, this);
 
+    // Берем текущий активный маршрут и раскрашиваем его.
     this.activeRoute = multiRoute.getActiveRoute();
     this.colorize();
 }
 
+// Пресет внешнего вида для сегментов электричек.
 Colorizer.suburbanPreset = {
     strokeWidth: 6,
     strokeColor: "#000000",
     strokeStyle: "shortDash"
 };
 
+// Пресет внешнего вида для сегментов автобусов, троллейбусов и трамваев.
 Colorizer.busPreset = {
     strokeWidth: 6,
     strokeColor: "#0085FA",
     strokeStyle: "shortDash"
 };
 
+// Пресет внешнего вида для пеших сегментов.
 Colorizer.walkPreset = {
     strokeWidth: 2,
     strokeColor: "#333333",
@@ -28,21 +36,29 @@ Colorizer.walkPreset = {
 };
 
 Colorizer.prototype = {
+    // Обработчик смены активного маршрута.
     onActiveRouteChange: function () {
+        // Снимаем раскраску с предыдущего активного маршрута.
         this.uncolorize();
+        // Запоминаем новый активный маршрут и раскрашиваем его.
         this.activeRoute = this.multiRoute.getActiveRoute();
         this.colorize();
 
         if (this.activeRoute) {
+            // Применяем к карте границы активного маршрута.
             this.multiRoute.getMap().setBounds(this.activeRoute.getBounds());
         }
     },
 
     onMultiRouteUpdate: function () {
+        // При обновлении маршрута выполняем раскрашивание заново.
         this.colorize();
     },
 
     colorize: function () {
+        // Если есть активный маршрут, перебираем все пути маршрута.
+        // Для каждого пути перебираем все сегменты и устанавливаем
+        // каждому из них параметры внешнего вида в зависимости от типа сегмента.
         if (this.activeRoute) {
             this.activeRoute.getPaths().each(function (path) {
                 path.getSegments().each(function (segment) {
@@ -50,6 +66,8 @@ Colorizer.prototype = {
                     if (segmentType == "transport") {
                         this.colorizeTransportSegment(segment);
                     } else {
+                        // Сюда попадут сегменты с типами "walk" (пешеходный)
+                        // и "transfer" (переход между станциями).
                         segment.options.set({ preset: Colorizer.walkPreset });
                     }
                 }, this)
@@ -59,6 +77,7 @@ Colorizer.prototype = {
 
     uncolorize: function () {
         if (this.activeRoute) {
+            // Перебираем все сегменты и для каждого из них удаляем настройки внешнего вида.
             this.activeRoute.getPaths().each(function (path) {
                 path.getSegments().each(function (segment) {
                     segment.options.unset("preset")
@@ -67,7 +86,14 @@ Colorizer.prototype = {
         }
     },
 
+    // Метод, раскрашивающий транспортные сегменты.
     colorizeTransportSegment: function (segment) {
+        // Берем первую запись о транспорте из массива транспортных средств сегмента
+        // и на ее основе раскрашиваем сегмент.
+        // Как правило для одного сегмента тип возможных транспортных средств не
+        // отличается радикально.
+        // Т.е. не бывает сегментов, по которым можно проехать как на метро, так и на автобусе.
+        // Различие же между автобусом и маршрутным такси в нашем случае неважно.
         var transport = segment.properties.get("transports")[0];
         if (transport.type == "suburban") {
             segment.options.set({ preset: Colorizer.suburbanPreset });
@@ -75,6 +101,7 @@ Colorizer.prototype = {
             segment.options.set({
                 preset: {
                     strokeWidth: 6,
+                    // Цвет линии метро берем из данных сегмента.
                     strokeColor: transport.Style.color
                 }
             });
