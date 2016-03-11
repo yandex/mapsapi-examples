204aced8845c0c7ef1c727d35822a26868592d3b
Alexey Sazonov <effrenus@gmail.com>
2016-03-11 13:02:57 +0300

    Custom map: update comments

--- custom_map/custom_map.js
+++ custom_map/custom_map.js
@@ -2,40 +2,40 @@ ymaps.ready(function () {
 
     var LAYER_NAME = 'user#layer',
         MAP_TYPE_NAME = 'user#customMap',
-        TILES_PATH = './images/tiles'; // директория с тайлами
+        TILES_PATH = './images/tiles'; // Директория с тайлами.
 
     /**
-     * Конструктор, создающий собственный слой
+     * Конструктор, создающий собственный слой.
      */
     var Layer = function () {
         var layer = new ymaps.Layer(TILES_PATH + '/%z/%x-%y.jpeg', {
-            // Если тайл не загрузился, показываем это изображение
+            // Если тайл не загрузился, показываем это изображение.
             notFoundTile: TILES_PATH + '/2/0-0.jpeg'
         });
-        // Указываем доступный диапазон масштабов для данного слоя
+        // Указываем доступный диапазон масштабов для данного слоя.
         layer.getZoomRange = function () {
             return ymaps.vow.resolve([1, 3]);
         };
-        // Добавляем свои копирайты
+        // Добавляем свои копирайты.
         layer.getCopyrights = function () {
             return ymaps.vow.resolve('©');
         };
         return layer;
     };
-    // Добавляем в хранилище слоев свой конструктор
+    // Добавляем в хранилище слоев свой конструктор.
     ymaps.layer.storage.add(LAYER_NAME, Layer);
 
     /**
-     * Создадим новый тип карты
-     * MAP_TYPE_NAME - имя нового типа
-     * LAYER_NAME - ключ в хранилище слоев или функция конструктор
+     * Создадим новый тип карты.
+     * MAP_TYPE_NAME - имя нового типа.
+     * LAYER_NAME - ключ в хранилище слоев или функция конструктор.
      */
     var mapType = new ymaps.MapType(MAP_TYPE_NAME, [LAYER_NAME]);
-    // Сохраняем тип в хранилище типов
+    // Сохраняем тип в хранилище типов.
     ymaps.mapType.storage.add(MAP_TYPE_NAME, mapType);
 
     /**
-     * Создаем карту, указав свой новый тип карты
+     * Создаем карту, указав свой новый тип карты.
      */
     var map = new ymaps.Map('map', {
         center: [0, 0],
@@ -43,7 +43,7 @@ ymaps.ready(function () {
         controls: ['zoomControl'],
         type: MAP_TYPE_NAME
     }, {
-        // Задаем в качестве проекции Декартову
+        // Задаем в качестве проекции Декартову.
         projection: new ymaps.projection.Cartesian([[-10, -10], [10, 10]], [false, false])
     });
 
