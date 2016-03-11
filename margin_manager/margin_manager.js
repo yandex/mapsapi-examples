cf5a8bb36003d1b73bd7d070ce7d7f1eb904f833
Alexey Sazonov <effrenus@gmail.com>
2016-03-11 13:00:10 +0300

    Margin manager: codestyle, rename helper plus description

--- margin_manager/margin_manager.js
+++ margin_manager/margin_manager.js
@@ -1,5 +1,5 @@
 ymaps.ready(function () {
-    var balloonPosition = [55.83866, 37.712326],
+    var balloonPosition = [55.83866, 37.712326], // Позиция балуна.
         Layout = ymaps.templateLayoutFactory.createClass([
             'Центровать<br>',
             '<button type="button" class="no-margin">без отступов</button>',
@@ -16,33 +16,32 @@ ymaps.ready(function () {
                 });
             }
         }),
-        map = new ymaps.Map(
-        'map',
-        {
-            center: [55.85, 37.7124],
-            zoom: 11,
-            controls: []
-        },
-        {
-            balloonContentLayout: Layout,
-            balloonAutoPan: false,
-            balloonPanelMaxMapArea: 0,
-            balloonCloseButton: false
-        }
-    );
+        map = new ymaps.Map('map',
+            {
+                center: [55.85, 37.7124],
+                zoom: 11,
+                controls: []
+            },
+            {
+                balloonContentLayout: Layout,
+                balloonAutoPan: false,
+                balloonPanelMaxMapArea: 0,
+                balloonCloseButton: false
+            }
+        );
 
-    // указываем для элементов область занимаемую над картой (положение и размер)
-    // поддерживаются значения в пикселях (px) и процентах (%)
-    // если единица измерения не указана, то считается, что значение в пикселях
+    // Указываем для элементов область занимаемую над картой (положение и размер).
+    // Поддерживаются значения в пикселях (px) и процентах (%),
+    // если единица измерения не указана, то считается, что значение в пикселях.
     var mapAreas = [
-        // панель слева
+        // Панель слева.
         {
             top: 0,
             left: 0,
             width: '80px',
-            height: '100%' // проценты расчитываются относительно размеров контейнера с картой
+            height: '100%' // Проценты расчитываются относительно размеров контейнера с картой.
         },
-        // блок в правом углу
+        // Блок в правом углу.
         {
             top: 10,
             right: 10,
@@ -50,20 +49,20 @@ ymaps.ready(function () {
             height: '40%'
         }
     ];
-    // добавляем каждый блок в менеджер отступов
+    // Добавляем каждый блок в менеджер отступов.
     mapAreas.forEach(function (area) {
-        // метод `addArea` менеджера отступов возвращает объект (аксессор), который предоставляет доступ к прямоугольной области в менеджере отступов
+        // Метод `addArea` менеджера отступов возвращает объект (аксессор), который предоставляет доступ к прямоугольной области в менеджере отступов.
         var accessor = map.margin.addArea(area);
-        // если у аксессора вызвать метод `remove`, то область будет удалена и менеджера отступов
-        // пример: accessor.remove()
+        // Если у аксессора вызвать метод `remove`, то область будет удалена из менеджера отступов.
+        // Пример: accessor.remove()
 
         addArea(accessor);
     });
 
     map.balloon.open(balloonPosition);
 
-    // Контролы поддерживают опцию adjustMapMargin
-    // Когда значение true, контрол автоматически добавляет свои размеры в менеджер отступов
+    // Контролы поддерживают опцию adjustMapMargin.
+    // Когда значение true, контрол автоматически добавляет свои размеры в менеджер отступов.
     var toggleAreaBtn = new ymaps.control.Button({
         data: {content: 'Показать занятые области', title: 'Показать все занятые области из менеджера отступов'},
         options: {
@@ -71,8 +70,8 @@ ymaps.ready(function () {
             // adjustMapMargin: true
         }
     });
-    // по клику над картой отображаются все области, добавленные
-    // в менеджер отступов
+    // По клику над картой отображаются все области, добавленные
+    // в менеджер отступов.
     toggleAreaBtn.events.add(['select', 'deselect'], function (event) {
         var container = document.getElementsByClassName('area-holder')[0];
         container && container.classList[event.originalEvent.type == 'select' ? 'remove' : 'add']('is-hidden');
@@ -82,7 +81,7 @@ ymaps.ready(function () {
     var toggleMarginBtn = new ymaps.control.Button({
         data: {content: 'Показать отступы', title: 'Показать отступы карты'},
         options: {
-            // разрешаем контролу автоматически добавить свои размеры в менеджер отступов
+            // Разрешаем контролу автоматически добавить свои размеры в менеджер отступов.
             // adjustMapMargin: true,
             maxWidth: 200
         }
@@ -93,7 +92,7 @@ ymaps.ready(function () {
     });
     map.controls.add(toggleMarginBtn);
 
-    // показываем отступы карты
+    // Показываем отступы карты.
     function updateMapMargins () {
         var margin = map.margin.getMargin();
         document.getElementsByClassName('map-bounds')[0].style.borderWidth = margin.join('px ') + 'px';
