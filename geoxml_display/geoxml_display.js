f8b47038fbc9b1a85565d1d1887cec46a943cc19
Ajaxy <alx.zinchuk@gmail.com>
2016-03-21 16:58:26 +0300

    fixes for https support

--- geoxml_display/geoxml_display.js
+++ geoxml_display/geoxml_display.js
@@ -16,12 +16,12 @@ function init() {
     // When the button is clicked, we load the appropriate XML file and display its data on the
     // map.
     ymapsmlButton.click(function (e) {
-        ymaps.geoXml.load('https://maps.yandex.ru/export/usermaps/93jfWjoXws37exPmKH-OFIuj3IQduHal/')
+        ymaps.geoXml.load('http://maps.yandex.ru/export/usermaps/93jfWjoXws37exPmKH-OFIuj3IQduHal/')
             .then(onGeoXmlLoad);
         e.target.disabled = true;
     });
     kmlButton.click(function (e) {
-        ymaps.geoXml.load('https://openflights.org/demo/openflights-sample.kml')
+        ymaps.geoXml.load('http://openflights.org/demo/openflights-sample.kml')
             .then(onGeoXmlLoad);
         e.target.disabled = true;
     });
