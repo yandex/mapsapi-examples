113cca15049b38786d3ea4ca952b3fd8269a4f4e
sergeiiak <sergeiiak@yandex-team.ru>
2016-07-18 11:17:53 +0300

    Добавил пример с азимутальной проекцией. Добавил запятую.

---
+++ azimuth_map/azimuth.js
@@ -0,0 +1,91 @@
+ymaps.modules.define('projection.Azimuth', [
+    'util.defineClass',
+    'util.math.cycleRestrict',
+    'coordSystem.Cartesian'
+], function (provide, defineClass, cycleRestrict, CoordSystemCartesian) {
+    /**
+     * @fileOverview
+     * Азимутальная проекция.
+     */
+
+    var latLongOrder = ymaps.meta.coordinatesOrder == 'latlong';
+
+    /**
+     * Создает азимутальную проекцию.
+     * Размер области в пикселях всегда NxN, где N = 256 * 2^zoom.
+     *
+     * @name projection.Azimuth
+     * @class Азимутальная проекция.
+     * Учитывает параметр coordorder загрузки API.
+     * @augments IProjection
+     * @param {Object[]} [center=[128, 128]] Массив из пиксельных координат центра карты (северный или южный географический полюс).
+     * @param {Number}  [latRatio=0.71111111111111] Количество градусов широты содержащееся в 1 пикселе на 0-м зуме.
+     * @param {Number}  [offsetAngle=0] Положительный угол смещения нулевого меридиана на карте по часовой стрелке.
+     * @param {Number}  [southPole=false] Cеверный или южный географический полюс, true - если северный.
+     * @param {Number|Number[]} [scale=1] Масштаб одного деления на оси. Может быть числом или парой чисел для каждой из осей.
+     */
+    function Azimuth(center, latRatio, offsetAngle, southPole, scale) {
+        if (ymaps.meta.debug) {
+            if (!center[0] || !center[1]) {
+                throw new Error("projection.Azimuth: Некорректные значения параметра center. Координаты центра должны быть заданы.");
+            }
+        }
+
+        this._mapPixelCenter = center ? center : [128, 128];
+
+        this._southPole = southPole ? southPole : false;
+
+        this._latRatio = latRatio ? latRatio : 0.71111111111111;
+
+        this._offsetAngle = offsetAngle ? offsetAngle : 0;
+
+        this._coordSystem = new CoordSystemCartesian(scale);
+    }
+
+    defineClass(Azimuth, {
+        toGlobalPixels: function (point, zoom) {
+            if (ymaps.meta.debug) {
+                if (!point) {
+                    throw new Error("Azimuth.toGlobalPixels: не передан параметр point");
+                }
+                if (typeof zoom == "undefined") {
+                    throw new Error("Azimuth.toGlobalPixels: не передан параметр zoom");
+                }
+            }
+
+            var mapPixelCenter = this._mapPixelCenter,
+                latRatio = this._latRatio,
+                southPole = this._southPole,
+                offsetAngle = this._offsetAngle,
+                longitude = cycleRestrict(point[latLongOrder ? 1 : 0] + offsetAngle, -180, 180),
+                latitude = point[latLongOrder ? 0 : 1],
+                centerX = mapPixelCenter[0] * Math.pow(2, zoom),
+                centerY = mapPixelCenter[1] * Math.pow(2, zoom),
+                radius = ((southPole ? -90 : 90) - latitude) * Math.pow(2, zoom) * latRatio,
+                x = centerX + radius * Math.sin(longitude * Math.PI / 180),
+                y = centerY + radius * Math.cos(longitude * Math.PI / 180);
+            return [x, y];
+        },
+
+        fromGlobalPixels: function (point, zoom) {
+            var x = point[0],
+                y = point[1],
+                mapPixelCenter = this._mapPixelCenter,
+                latRatio = this._latRatio,
+                southPole = this._southPole,
+                offsetAngle = this._offsetAngle,
+                centerX = mapPixelCenter[0] * Math.pow(2, zoom),
+                centerY = mapPixelCenter[1] * Math.pow(2, zoom),
+                longitude = cycleRestrict(Math.atan2(x - centerX, y - centerY) * 180 / Math.PI + offsetAngle, -180, 180),
+                latitude = 90 - ( Math.sqrt(Math.pow(x - centerX, 2) + Math.pow(y - centerY, 2)) / (Math.pow(2, zoom) * latRatio)),
+                latitude = (southPole ? -latitude : latitude);
+            return latLongOrder ? [latitude, longitude] : [longitude, latitude];
+        },
+
+        isCycled: function () {
+            return [false, false];
+        }
+    });
+
+    provide(Azimuth);
+});
---
+++ azimuth_map/projection_map.js
@@ -0,0 +1,64 @@
+ymaps.ready(['projection.Azimuth']).then(function init () {
+
+    var ARCTIC_LAYER_NAME = 'user#arcticLayer',
+        ARCTIC_MAP_TYPE_NAME = 'Арктика',
+        ARCTIC_TILES_PATH = 'images/tiles_arctic',
+
+    /**
+     * Конструктор, создающий собственный слой.
+     */
+    ArcticLayer = function () {
+        var layer = new ymaps.Layer(ARCTIC_TILES_PATH + '/%z/tile-%x-%y.jpg', {
+                // Если тайл не загрузился, показываем это изображение.
+                notFoundTile: ARCTIC_TILES_PATH + '/3/tile-0-0.jpg'
+            });
+        // Указываем доступный диапазон масштабов для данного слоя.
+        layer.getZoomRange = function () {
+            return ymaps.vow.resolve([0, 3]);
+        };
+        return layer;
+    };
+
+    // Добавляем в хранилище слоев свой конструктор.
+    ymaps.layer.storage.add(ARCTIC_LAYER_NAME, ArcticLayer);
+
+    /**
+     * Создадим новый тип карты.
+     * MAP_TYPE_NAME - имя нового типа.
+     * LAYER_NAME - ключ в хранилище слоев или функция конструктор.
+     */
+    var mapType = new ymaps.MapType(ARCTIC_MAP_TYPE_NAME, [ARCTIC_LAYER_NAME]);
+    // Сохраняем тип в хранилище типов.
+    ymaps.mapType.storage.add(ARCTIC_MAP_TYPE_NAME, mapType);
+
+    /**
+     * Создаем карту, указав свой новый тип карты.
+     */
+    var map = new ymaps.Map('map', {
+        center: [90, 0],
+        zoom: 1,
+        controls: ["searchControl"],
+        type: ARCTIC_MAP_TYPE_NAME
+    }, {
+        // Задаем азимутальную проекцию.
+        projection: new ymaps.projection.Azimuth()
+    }), regions;
+
+    var regionsButton = new ymaps.control.Button({data: {content: 'Добавить регионы'}, options: {selectOnClick: true}});
+    regionsButton.events
+        .add('select', function () {
+            map.geoObjects.add(regions.geoObjects);
+        })
+        .add('deselect', function () {
+            map.geoObjects.remove(regions.geoObjects);
+        });
+
+    ymaps.regions.load('001', {
+        lang: 'ru'
+    }).then(function (result) {
+        regions = result;
+        map.controls.add(regionsButton);
+        regionsButton.options.set('maxWidth', 150);
+    });
+
+});
