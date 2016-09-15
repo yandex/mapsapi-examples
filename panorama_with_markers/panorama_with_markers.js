31439bde028add5906a72ed70dcbc1f8029f305e
Olesya Gorbacheva <ogorbacheva@yandex-team.ru>
2016-09-15 19:12:04 +0300

    Добавление нового примера панорамы с маркерами

---
+++ panorama_with_markers/panorama_with_markers.js
@@ -0,0 +1,149 @@
+ymaps.ready(function () {
+    // Для начала проверим, поддерживает ли плеер браузер пользователя.
+    if (!ymaps.panorama.isSupported()) {
+        // Если нет, то ничего не будем делать.
+        return;
+    }
+    // Создаем объект, который будет содержать данные о нашей панораме.
+    // Панорама имеет неполный угол обзора по вертикали.
+    var angularBBox = [0.35, 8 * Math.PI/3, -0.61, 2 * Math.PI/3],
+        position = [0, 0, 0],
+        tileSize = [512, 512],
+        tileLevels = [{
+            // URL тайлов для высокого уровня детализации.
+            getTileUrl: function (x, y) {
+                return '/tiles/hq/' + x + '-' + y + '.jpg';
+            },
+            // Размер изображения для высокого уровня детализации.
+            getImageSize: function () {
+                return [10752, 1658];
+            }
+        }, {
+            // URL тайлов для низкого уровня детализации.
+            getTileUrl: function (x, y) {
+                return '/tiles/lq/' + x + '-' + y + '.jpg';
+            },
+            // Размер изображения для низкого уровня детализации.
+            getImageSize: function () {
+                return [1024, 158];
+            }
+        }],
+
+        // Создаем объект, в котором будет храниться
+        // информация о маркере.
+        markerData = {
+            src: {
+                'default': '/images/marker-default.png',
+                hovered: '/images/marker-hovered.png'
+            },
+            position: [3, -11, 0.9]
+        };
+
+    // Функция возвращает Promise-объект, который будет разрешен
+    // объектом HTMLImageElement.
+    function loadImage(src) {
+        return new ymaps.vow.Promise(function (resolve) {
+            var image = new Image();
+            image.onload = function () {
+                resolve(image);
+            };
+            image.crossOrigin = 'anonymous';
+            image.src = src;
+        });
+    }
+
+    // Функция, которая рисует маркер с помощью Canvas.
+    function renderImage(text) {
+        var ctx = document.createElement('canvas').getContext('2d');
+        ctx.canvas.width = 128;
+        ctx.canvas.height = 32;
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, 128, 32);
+        ctx.fillStyle = 'black';
+        ctx.font = '24px Arial';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillText(text, 64, 16);
+        return ctx.canvas;
+    }
+
+    // Класс маркера.
+    function Marker(img, panorama) {
+        // В классе должно быть определено поле properties.
+        this.properties = new ymaps.data.Manager();
+        this._panorama = panorama;
+        this._position = img.position;
+        this._imgSrc = img.src;
+    }
+
+    // Определяем в классе Marker нужные методы.
+    ymaps.util.defineClass(Marker, {
+        getIconSet: function () {
+            return ymaps.vow.Promise.all([
+                loadImage(this._imgSrc.default),
+                loadImage(this._imgSrc.hovered)
+            ]).spread(function (defaultImage, hoveredImage) {
+                return {
+                    'default': {
+                        image: defaultImage,
+                        offset: [0, 0]
+                    },
+                    hovered: {
+                        image: hoveredImage,
+                        offset: [0, 0]
+                    },
+                    expanded: {
+                        image: renderImage("JavaScript"),
+                        offset: [0, 0]
+                    }
+                };
+            });
+        },
+        getPanorama: function () {
+            return this._panorama;
+        },
+        getPosition: function () {
+            return this._position;
+        }
+    });
+
+    // Объявляем класс панорамы.
+    function MyPanorama(angularBBox, position, tileSize, tileLevels) {
+        // Вызываем конструктор родительского класса.
+        ymaps.panorama.Base.call(this);
+        this._angularBBox = angularBBox;
+        this._position = position;
+        this._tileSize = tileSize;
+        this._tileLevels = tileLevels;
+
+    }
+
+    ymaps.util.defineClass(MyPanorama, ymaps.panorama.Base, {
+        // Переопределяем метод getMarkers.
+        getMarkers: function () {
+            return [new Marker(markerData, this)];
+        },
+        getAngularBBox: function () {
+            return this._angularBBox;
+        },
+        getPosition: function () {
+            return this._position;
+        },
+        getTileSize: function () {
+            return this._tileSize;
+        },
+        getTileLevels: function () {
+            return this._tileLevels;
+        },
+        getCoordSystem: function () {
+            return ymaps.coordSystem.cartesian;
+        }
+    });
+
+    var panorama = new MyPanorama(angularBBox, position, tileSize, tileLevels);
+
+    // Отображаем панораму на странице.
+    var player = new ymaps.panorama.Player('player', panorama, {
+        direction: [290, 30]
+    });
+})
