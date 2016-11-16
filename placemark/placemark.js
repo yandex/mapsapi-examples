ad7ea5da8a0cd471e8d1e1840ae146cf11c7ec3a
Olesya Gorbacheva <ogorbacheva@yandex-team.ru>
2016-11-16 17:07:41 +0300

    fixes in example 2.0

--- placemark/placemark.js
+++ placemark/placemark.js
@@ -46,7 +46,7 @@ function init () {
         }, {
             // Опции.
             // Своё изображение иконки метки.
-            iconImageHref: '/images/myIcon.gif',
+            iconImageHref: 'images/myIcon.gif',
             // Размеры метки.
             iconImageSize: [30, 42],
             // Смещение левого верхнего угла иконки относительно
