90a80ca34dc5ad65fe4f8902decd81c0306a3221
sergeiiak <sergeiiak@yandex-team.ru>
2016-11-20 23:05:55 +0300

    11890 : добавил kml и gpx

--- geoxml_display/geoxml_display.js
+++ geoxml_display/geoxml_display.js
@@ -2,26 +2,26 @@ ymaps.ready(init);
 
 function init() {
     var myMap = new ymaps.Map('map', {
-            center: [55.76, 37.64],// Москва
-            zoom: 2,
+            center: [59.994675, 29.702651],// Москва
+            zoom: 10,
             controls: ['zoomControl']
         }),
-        ymapsmlButton = $('.load-ymapsml'),
+        gpxButton = $('.load-gpx'),
         kmlButton = $('.load-kml');
 
     // Отключение кеширования атрибута disabled в Firefox.
-    ymapsmlButton.get(0).disabled = false;
+    gpxButton.get(0).disabled = false;
     kmlButton.get(0).disabled = false;
 
     // При нажатии на кнопку загружаем соответствующий XML-файл.
     // и отображаем его данные на карте.
-    ymapsmlButton.click(function (e) {
-        ymaps.geoXml.load('http://maps.yandex.ru/export/usermaps/93jfWjoXws37exPmKH-OFIuj3IQduHal/')
+   gpxButton.click(function (e) {
+        ymaps.geoXml.load('geoObjects.gpx')
             .then(onGeoXmlLoad);
         e.target.disabled = true;
     });
     kmlButton.click(function (e) {
-        ymaps.geoXml.load('http://openflights.org/demo/openflights-sample.kml')
+        ymaps.geoXml.load('geoObjects.kml')
             .then(onGeoXmlLoad);
         e.target.disabled = true;
     });
