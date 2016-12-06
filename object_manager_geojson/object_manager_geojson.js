fb7b9cdea82d7f424d060021f7aaf25bce8a9e73
sergeiiak <sergeiiak@yandex-team.ru>
2016-12-06 11:42:52 +0300

    правки по ревью

--- object_manager_geojson/object_manager_geojson.js
+++ object_manager_geojson/object_manager_geojson.js
@@ -1,21 +1,21 @@
 ymaps.ready(function () {
 
     var map = new ymaps.Map('map', {
-        center: [29.902651, 60.02],
-        zoom: 11,
-        controls: ['zoomControl']
-    }),
+            center: [29.902651, 60.02],
+            zoom: 11,
+            controls: ['zoomControl']
+        }),
         objectManager = new ymaps.ObjectManager();
 
     // Загружаем GeoJSON файл экспортированный из Конструктора карт.
     $.getJSON('geoObjects.geojson')
         .done(function (geoJson) {
 
-            geoJson.features.forEach(function(obj){
+            geoJson.features.forEach(function (obj) {
                 // Задаём контент балуна.
                 obj.properties.balloonContent = obj.properties.name;
                 // Задаём пресет для меток с полем iconCaption.
-                if(obj.properties.iconCaption){
+                if (obj.properties.iconCaption) {
                     obj.options = {
                         preset: "islands#greenDotIconWithCaption"
                     }
