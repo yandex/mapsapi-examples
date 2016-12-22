1fbfd2d5abc92ac245a2dfdcbdd27609c7ba53f4
Olesya Gorbacheva <ogorbacheva@yandex-team.ru>
2016-12-22 18:47:08 +0300

    fix code after translation

--- find_closest_object/find_closest_object.js
+++ find_closest_object/find_closest_object.js
@@ -11,18 +11,21 @@ function init() {
     
     function findClosestObjects () {
         // Searching the selection for the cafe nearest to the found metro station, and opening its
-        // balloon.        cafe.getClosestTo(metro.get(0)).balloon.open();
+        // balloon.
+        cafe.getClosestTo(metro.get(0)).balloon.open();
         
         // Opening the balloon of the cafe that is closest to the click.       
-        // myMap.events.add('click', function (event) {
+        myMap.events.add('click', function (event) {
             cafe.getClosestTo(event.get('coords')).balloon.open();
         });
     }
     
-    ///Cafe descriptions can be stored in JSON format, in order to then generate geo objects from the descriptions using ymaps.geoQuery.    cafe = ymaps.geoQuery({
+    // Cafe descriptions can be stored in JSON format, in order to then generate geo objects
+    // from the descriptions using ymaps.geoQuery.
+    cafe = ymaps.geoQuery({
         type: 'FeatureCollection',
         features: [{
-                type: 'Feature',
+            type: 'Feature',
                 properties: {
                     balloonContent: 'Кофейня "Дарт Вейдер" - у нас есть печеньки!'
                 },
@@ -50,10 +53,10 @@ function init() {
                 }
             }
         ]
-    // Adding points to the map immediately.    }).addToMap(myMap);
+    }).addToMap(myMap); // Adding points to the map immediately.
 
-    // Using reverse geocoding to find the metro station "Kropotkinskaya".    metro =
-    // ymaps.geoQuery(ymaps.geocode([55.744828, 37.603423], {kind: 'metro'}))
+    // Using reverse geocoding to find the metro station "Kropotkinskaya".
+    metro = ymaps.geoQuery(ymaps.geocode([55.744828, 37.603423], { kind: 'metro' }))
     // Waiting for the response from the server and then processing the received results.       
-    // .then(findClosestObjects);
+        .then(findClosestObjects);
 }
