2dbded9780848d4b27287ee7964dbefc318ceea8
sergeiiak <sergeiiak@yandex-team.ru>
2017-07-04 15:04:09 +0100

    12874 : add new example

---
+++ mapsapi-area/mapsapi-area.js
@@ -0,0 +1,37 @@
+ymaps.ready(['util.calculateArea']).then(function () {
+    var myMap = new ymaps.Map("map", {
+            center: [55.76, 37.64],
+            zoom: 18,
+            controls: ['searchControl', 'zoomControl']
+        }, {
+            searchControlProvider: 'yandex#search'
+        }), placemark,
+    // Создаем многоугольник без вершин.
+        polygon = new ymaps.Polygon([]);
+    // Добавляем многоугольник на карту.
+    myMap.geoObjects.add(polygon);
+    // Включаем режим редактирования с возможностью добавления новых вершин.
+    polygon.editor.startDrawing();
+
+    // Подписываемся на изменение координат.
+    polygon.geometry.events.add('change', function () {
+        // Вычисляем площадь многоугольника.
+        var area = Math.round(ymaps.util.calculateArea(polygon)) + ' кв.м',
+            center;
+        // Проверяем, что пользователь поставил первую точку полигона.
+        if (polygon.geometry.get(0)[0]) {
+            // Вычисляем центр для добавления метки.
+            center = ymaps.util.bounds.getCenter(polygon.geometry.getBounds());
+        }
+        if (center) {
+            if (!placemark) {
+                placemark = new ymaps.Placemark(center, {'iconCaption': area}, {preset: 'islands#greenDotIconWithCaption'});
+                myMap.geoObjects.add(placemark);
+            }
+            else {
+                placemark.geometry.setCoordinates(center);
+                placemark.properties.set('iconCaption', area);
+            }
+        }
+    });
+});
