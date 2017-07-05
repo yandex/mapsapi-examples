949f171811ffc5b331f43cbb8a7ef1b37f2eedc4
sergeiiak <sergeiiak@yandex-team.ru>
2017-07-05 10:43:41 +0100

    12874 : change 'кв. м' on 'м²'

--- mapsapi-area/mapsapi-area.js
+++ mapsapi-area/mapsapi-area.js
@@ -18,7 +18,7 @@ ymaps.ready(['util.calculateArea']).then(function () {
         // Вычисляем площадь многоугольника.
         var area = Math.round(ymaps.util.calculateArea(polygon)),
             center;
-        // Если площадь превышает 1 000 000 кв. м, то приводим её к кв. км
+        // Если площадь превышает 1 000 000 м², то приводим её к км²
         if (area <= 1e6) {
             area += ' м²';
         } else {
