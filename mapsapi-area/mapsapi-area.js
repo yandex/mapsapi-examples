557f29531e93fb47b50ca1dd8f55be392fc5a801
sergeiiak <sergeiiak@yandex-team.ru>
2017-07-11 10:54:11 +0100

    Переделал пример.

--- mapsapi-area/mapsapi-area.js
+++ mapsapi-area/mapsapi-area.js
@@ -1,59 +1,41 @@
+// Обратите внимание, что модуль не умеет обсчитывать полигоны с самопересечениями.
 ymaps.ready(['util.calculateArea']).then(function () {
     var myMap = new ymaps.Map("map", {
-            center: [37.64, 55.76],
-            zoom: 18,
+            center: [55.9238145091058, 37.897131347654376],
+            zoom: 10,
             controls: ['searchControl', 'zoomControl']
         }, {
             searchControlProvider: 'yandex#search'
-        }), placemark,
-    // Создаем многоугольник без вершин.
-        polygon = new ymaps.Polygon([]);
-    // Добавляем многоугольник на карту.
-    myMap.geoObjects.add(polygon);
-    // Включаем режим редактирования с возможностью добавления новых вершин.
-    polygon.editor.startDrawing();
-
-    // Подписываемся на изменение координат.
-    polygon.geometry.events.add('change', function () {
-        // Проверяем, что пользователь поставил первую точку полигона.
-        if (polygon.geometry.get(0)[0]) {
-            // Вычисляем центр для добавления метки.
-            center = ymaps.util.bounds.getCenter(polygon.geometry.getBounds());
-        }
-        // Обратите внимание, что модуль не умеет обсчитывать полигоны с самопересечениями.
-        // Находим количество самопересечений.
-        var kinks = turf.kinks({
-            "type": "Feature",
-            "properties": {},
-            "geometry": {
-                "type": "Polygon",
-                "coordinates": polygon.geometry.getCoordinates()
+        }),
+        // Создаем многоугольник, круг и прямоугольник.
+        polygon = new ymaps.GeoObject({
+            geometry: {
+                type: "Polygon", coordinates: [[
+                    [55.97544201439153, 37.71984863281182],
+                    [55.876808118310706, 37.66697692871001],
+                    [55.8748378377763, 37.78258361816322],
+                    [55.97544201439153, 37.71984863281182]
+                ]]
             }
-        });
-        // Если количество самопересечений больше нуля, то сообщаем об этом пользователю.
-        if (kinks.features.length > 0){
-            placemark.geometry.setCoordinates(center);
-            placemark.properties.set('iconCaption', 'Невозможно вычислить площадь');
-            return
-        }
-        // Вычисляем площадь многоугольника.
-        var area = Math.round(ymaps.util.calculateArea(polygon)),
-            center;
-        // Если площадь превышает 1 000 000 м², то приводим ее к км²
+        }),
+        circle = new ymaps.Circle([[55.9238145091058, 38.097131347654376], 5000]),
+        rectangle = new ymaps.Rectangle([
+            [55.973805634187, 37.81389007567776],
+            [55.87510965298843, 37.95396575927215]
+        ]);
+    // Добавляем геообъекты на карту.
+    myMap.geoObjects.add(polygon).add(circle).add(rectangle);
+
+    myMap.geoObjects.events.add('click', function (e) {
+        var geoObject = e.get('target');
+        // Вычисляем площадь геообъекта.
+        var area = Math.round(ymaps.util.calculateArea(geoObject));
+        // Если площадь превышает 1 000 000 м², то приводим ее к км².
         if (area <= 1e6) {
             area += ' м²';
         } else {
             area = (area / 1e6).toFixed(3) + ' км²';
         }
-        if (center) {
-            if (!placemark) {
-                placemark = new ymaps.Placemark(center, {'iconCaption': area}, {preset: 'islands#greenDotIconWithCaption'});
-                myMap.geoObjects.add(placemark);
-            }
-            else {
-                placemark.geometry.setCoordinates(center);
-                placemark.properties.set('iconCaption', area);
-            }
-        }
+        geoObject.properties.set('balloonContent', area);
     });
 });
