f9b485a9a7698d9d3055b5b6a2ab85fb0cbdba51
sergeiiak <sergeiiak@yandex-team.ru>
2017-10-18 16:04:45 +0100

    11962: Пример на попадание точки в зону доставки

---
+++ delivery_zones/delivery_zones.js
@@ -0,0 +1,109 @@
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map('map', {
+            center: [30.264981955459618, 59.9567962610097],
+            zoom: 9,
+            controls: ['geolocationControl', 'searchControl']
+        }),
+        deliveryPoint = new ymaps.GeoObject({
+            geometry: {type: 'Point'},
+            properties: {iconCaption: 'Адрес'}
+        }, {
+            preset: 'islands#blackDotIconWithCaption',
+            draggable: true,
+            iconCaptionMaxWidth: '215'
+        }),
+        searchControl = myMap.controls.get('searchControl');
+    searchControl.options.set({noPlacemark: true, placeholderContent: 'Введите адрес доставки'});
+    myMap.geoObjects.add(deliveryPoint);
+
+    function onZonesLoad(json) {
+        // Добавляем зоны на карту.
+        var deliveryZones = ymaps.geoQuery(json).addToMap(myMap);
+        // Задаём цвет и контент балунов полигонов.
+        deliveryZones.each(function (obj) {
+            var color = obj.options.get('fillColor'),
+                color = color.substring(0, color.length - 2);
+            obj.options.set({fillColor: color, fillOpacity: 0.4});
+            obj.properties.set('balloonContent', obj.properties.get('name'));
+            obj.properties.set('balloonContentHeader', 'Стоимость доставки: ' + obj.properties.get('price') + ' р.')
+        });
+
+        // При поиске вызываем функцию выделения зоны доставки.
+        searchControl.events.add('resultshow', function () {
+            highlightResult(searchControl.getResultsArray()[0]);
+        });
+
+        // После геолокации вызываем функцию выделения зоны доставки.
+        myMap.controls.get('geolocationControl').events.add('locationchange', function (e) {
+            highlightResult(e.get('geoObjects').get(0));
+        });
+
+        // При перемещении метки сбрасываем подпись, содержимое балуна и перекрашиваем метку.
+        deliveryPoint.events.add('dragstart', function () {
+            deliveryPoint.properties.set({iconCaption: '', balloonContent: ''});
+            deliveryPoint.options.set('iconColor', 'black');
+        });
+
+        // По окончанию перемещения метки вызываем функцию выделения зоны доставки.
+        deliveryPoint.events.add('dragend', function () {
+            highlightResult(deliveryPoint);
+        });
+
+        function highlightResult(obj) {
+            // Сохраняем координаты переданного объекта.
+            var coords = obj.geometry.getCoordinates(),
+            // Находим полигон в который входят переданные координаты.
+                polygon = deliveryZones.searchContaining(coords).get(0);
+
+            if (polygon) {
+                // Уменьшаем прозрачность всех полигонов кроме того в который входят переданные координаты.
+                deliveryZones.setOptions('fillOpacity', 0.4);
+                polygon.options.set('fillOpacity', 0.8);
+                // Перемещаем метку с подписью в переданные координаты и перекрашиваем её в цвет полигона.
+                deliveryPoint.geometry.setCoordinates(coords);
+                deliveryPoint.options.set('iconColor', polygon.options.get('fillColor'));
+                // Задаем подпись для метки.
+                if (typeof(obj.getThoroughfare) === 'function') {
+                    var address = [obj.getThoroughfare(), obj.getPremiseNumber(), obj.getPremise()].join(' ');
+                    deliveryPoint.properties.set({iconCaption: address, balloonContent: address});
+                } else {
+                    // Если вы не хотите, чтобы при каждом перемещении метки отправлялся запрос к геокодеру,
+                    // закомментируйте код ниже.
+                    ymaps.geocode(coords, {results: 1}).then(function (res) {
+                        var obj = res.geoObjects.get(0),
+                            address = [obj.getThoroughfare(), obj.getPremiseNumber(), obj.getPremise()].join(' ');
+                        if (address.trim() == '') {
+                            address = obj.getAddressLine();
+                        }
+                        deliveryPoint.properties.set({
+                            iconCaption: address,
+                            balloonContent: address,
+                            balloonContentHeader: '<b>Стоимость доставки: ' + polygon.properties.get('price') + ' р.</b>'
+                        });
+                    });
+                }
+            } else {
+                // Если переданные координаты не попадают в полигон, то задаём стандартную прозрачность полигонов.
+                deliveryZones.setOptions('fillOpacity', 0.4);
+                // Перемещаем метку по переданным координатам.
+                deliveryPoint.geometry.setCoordinates(coords);
+                // Задаём контент балуна и метки.
+                deliveryPoint.properties.set({
+                    iconCaption: 'Доставка транспортной компанией',
+                    balloonContent: 'Cвяжитесь с оператором',
+                    balloonContentHeader: ''
+                });
+                // Перекрашиваем метку в чёрный цвет.
+                deliveryPoint.options.set('iconColor', 'black');
+            }
+        }
+    }
+
+    $.ajax({
+        url: 'zones.geojson',
+        dataType: 'json',
+        success: onZonesLoad
+    });
+}
