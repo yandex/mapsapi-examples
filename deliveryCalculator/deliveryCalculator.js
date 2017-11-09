2c5537609252fbb21ca54130e76d56337139f04d
sergeiiak <sergeiiak@yandex-team.ru>
2017-11-09 12:17:09 +0000

    Переделал пример "Расчет стоимости доставки" Поправил пару багов.

--- deliveryCalculator/deliveryCalculator.js
+++ deliveryCalculator/deliveryCalculator.js
@@ -1,80 +1,70 @@
-ymaps.ready(['DeliveryCalculator']).then(function init() {
-    var myMap = new ymaps.Map('map', {
+ymaps.ready(init);
+
+function init() {
+    // Стоимость за километр.
+    var DELIVERY_TARIFF = 20,
+    // Минимальная стоимость.
+        MINIMUM_COST = 500,
+        myMap = new ymaps.Map('map', {
             center: [60.906882, 30.067233],
             zoom: 9,
-            type: 'yandex#map',
             controls: []
         }),
-        searchStartPoint = new ymaps.control.SearchControl({
+    // Создадим панель маршрутизации.
+        routePanelControl = new ymaps.control.RoutePanel({
             options: {
-                useMapBounds: true,
-                noPlacemark: true,
-                noPopup: true,
-                placeholderContent: 'Адрес начальной точки',
-                size: 'large'
+                // Добавим заголовок панели.
+                showHeader: true,
+                title: 'Расчёт доставки'
             }
         }),
-        searchFinishPoint = new ymaps.control.SearchControl({
+        zoomControl = new ymaps.control.ZoomControl({
             options: {
-                useMapBounds: true,
-                noCentering: true,
-                noPopup: true,
-                noPlacemark: true,
-                placeholderContent: 'Адрес конечной точки',
-                size: 'large',
+                size: 'small',
                 float: 'none',
-                position: {left: 10, top: 44}
-            }
-        }),
-        calculator = new ymaps.DeliveryCalculator(myMap),
-        routeButton = new ymaps.control.Button({
-            data: {content: 'Москва-Петербург'},
-            options: {selectOnClick: false, maxWidth: 200}
-        });
-
-    myMap.controls.add(searchStartPoint)
-        .add(searchFinishPoint)
-        .add(routeButton, {float: 'none', position: {left: 10, bottom: 40}});
-
-    searchStartPoint.events
-        .add('resultselect', function (e) {
-            var results = searchStartPoint.getResultsArray(),
-                selected = e.get('index'),
-                point = results[selected].geometry.getCoordinates(),
-                balloonContent = results[selected].properties.get("balloonContent");
-
-            // Задаем начало маршрута.
-            calculator.setPoint("start", point, balloonContent);
-
-        })
-        .add('load', function (event) {
-            // По полю skip определяем, что это не дозагрузка данных.
-            // По getResultsCount определяем, что есть хотя бы 1 результат.
-            if (!event.get('skip') && searchStartPoint.getResultsCount()) {
-                searchStartPoint.showResult(0);
+                position: {
+                    bottom: 145,
+                    right: 10
+                }
             }
         });
+    // Пользователь сможет построить только автомобильный маршрут.
+    routePanelControl.routePanel.options.set({
+        types: {auto: true}
+    });
 
-    searchFinishPoint.events
-        .add('resultselect', function (e) {
-            var results = searchFinishPoint.getResultsArray(),
-                selected = e.get('index'),
-                point = results[selected].geometry.getCoordinates(),
-                balloonContent = results[selected].properties.get("balloonContent");
+    // Если вы хотите задать неизменяемую точку "откуда", раскомментируйте код ниже.
+    /*routePanelControl.routePanel.state.set({
+     fromEnabled: false,
+     from: 'Москва, Льва Толстого 16'
+     });*/
 
-            // Задаем конец маршрута.
-            calculator.setPoint("finish", point, balloonContent);
-        })
-        .add('load', function (event) {
-            // По полю skip определяем, что это не дозагрузка данных.
-            // По getResultsCount определяем, что есть хотя бы 1 результат.
-            if (!event.get('skip') && searchFinishPoint.getResultsCount()) {
-                searchFinishPoint.showResult(0);
+    myMap.controls.add(routePanelControl).add(zoomControl);
+
+    // Получим ссылку на маршрут.
+    routePanelControl.routePanel.getRouteAsync().then(function (route) {
+
+        // Повесим обработчик на событие построения маршрута.
+        route.model.events.add('requestsuccess', function () {
+
+            var activeRoute = route.getActiveRoute();
+            if (activeRoute) {
+                // Получим протяженность маршрута.
+                var length = route.getActiveRoute().properties.get("distance"),
+                // Вычислим стоимость доставки.
+                    price = calculate(Math.round(length.value / 1000)),
+                // Создаём макет содержимого балуна маршрута.
+                    balloonContentLayout = ymaps.templateLayoutFactory.createClass(
+                        '<span>Расстояние: ' + length.text + '.</span><br/>' +
+                        '<span style="font-weight: bold; font-style: italic">Стоимость доставки: ' + price + ' р.</span>');
+                // Задаём макет содержимого балуна маршрута.
+                route.options.set('routeBalloonContentLayout', balloonContentLayout);
             }
         });
 
-    routeButton.events
-        .add('click', function () {
-            calculator.setRoute([59.939095, 30.315868], [55.757026, 37.615032])
-        });
-});
+    });
+    // Вычисляем стоимость доставки.
+    function calculate(routeLength) {
+        return Math.max(routeLength * DELIVERY_TARIFF, MINIMUM_COST);
+    }
+}
