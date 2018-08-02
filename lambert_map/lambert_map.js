a04ca204b6b7fd3b11412d34babede57858d4566
sergeiiak <sergeiiak@yandex-team.ru>
2018-08-02 12:10:13 +0100

    12799 : равноугольная коническая проекция Ламберта для России

---
+++ lambert_map/lambert_map.js
@@ -0,0 +1,42 @@
+ymaps.ready(['projection.LambertConformalConic']).then(function init() {
+
+    // Создаём проекцию Ламберта.
+    var LAMBERT_PROJECTION = new ymaps.projection.LambertConformalConic();
+
+    // Создаём карту.
+    var map = myMap = new ymaps.Map('map', {
+        center: [60, 100],
+        zoom: 1,
+        type: null,
+        controls: ['zoomControl']
+    }, {
+        // Задаем проекцию Ламберта.
+        projection: LAMBERT_PROJECTION
+    });
+    myMap.controls.get('zoomControl').options.set({size: 'small'});
+
+    // Добавляем фон.
+    var pane = new ymaps.pane.StaticPane(map, {
+        zIndex: 100, css: {
+            width: '100%', height: '100%', backgroundColor: '#485668'
+        }
+    });
+    map.panes.append('greyBackground', pane);
+
+    // Загружаем и добавляем регионы России на карту.
+    ymaps.borders.load('RU', {
+        lang: 'ru'
+    }).then(function (result) {
+        regions = new ymaps.GeoObjectCollection(null, {
+            fillColor: '#051c3a',
+            strokeColor: '#9299a2',
+            hasHint: false,
+            cursor: 'default'
+        });
+        for (var i = 0; i < result.features.length; i++) {
+            regions.add(new ymaps.GeoObject(result.features[i]));
+        }
+
+        map.geoObjects.add(regions);
+    });
+});
