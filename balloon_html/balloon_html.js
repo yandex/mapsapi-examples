f22e1f57e2f03e43f1131ff7b782298dd47791bd
sergeiiak <sergeiiak@yandex-team.ru>
2018-08-08 11:33:39 +0100

    13749 : Пример на собственный балун

---
+++ balloon_html/balloon_html.js
@@ -0,0 +1,29 @@
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map('map', {
+        center: [55.751574, 37.573856],
+        zoom: 9,
+        controls: []
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+
+    var placemark = new ymaps.Placemark(myMap.getCenter(), {
+        // Зададим содержимое заголовка балуна.
+        balloonContentHeader: '<a href = "#">Рога и копыта</a><br>' +
+            '<span class="description">Сеть кинотеатров</span>',
+        // Зададим содержимое основой части балуна.
+        balloonContentBody: '<img src="img/cinema.jpg" height="150" width="200"> <br/> ' +
+            '<a href="tel:+7-123-456-78-90">+7 (123) 456-78-90</a><br/>' +
+            '<b>Ближайшие сеансы</b> <br/> Сеансов нет.',
+        // Зададим содержимое нижней части балуна.
+        balloonContentFooter: 'Информация предоставлена:<br/>OOO "Рога и копыта"',
+        // Зададим содержимое всплывающей подсказки.
+        hintContent: 'Рога и копыта'
+    });
+    // Добавим метку на карту.
+    myMap.geoObjects.add(placemark);
+    // Откроем балун на метке.
+    placemark.balloon.open();
+}
