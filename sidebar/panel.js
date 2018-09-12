a70c81b8238537f373174803783b49743483f1d4
se0ga <seoga2005@yandex.ru>
2018-09-12 23:05:49 +0100

    13790 - Пример с боковой панелью (#64)

---
+++ sidebar/panel.js
@@ -0,0 +1,55 @@
+// Пример реализации боковой панели на основе наследования от collection.Item.
+// Боковая панель отображает информацию, которую мы ей передали.
+ymaps.modules.define('Panel', [
+    'util.augment',
+    'collection.Item'
+], function (provide, augment, item) {
+    // Создаем собственный класс.
+    var Panel = function (options) {
+        Panel.superclass.constructor.call(this, options);
+    };
+
+    // И наследуем его от collection.Item.
+    augment(Panel, item, {
+        onAddToMap: function (map) {
+            Panel.superclass.onAddToMap.call(this, map);
+            this.getParent().getChildElement(this).then(this._onGetChildElement, this);
+            // Добавим отступы на карту.
+            // Отступы могут учитываться при установке текущей видимой области карты,
+            // чтобы добиться наилучшего отображения данных на карте.
+            map.margin.addArea({
+                top: 0,
+                left: 0,
+                width: '250px',
+                height: '100%'
+            })
+        },
+
+        onRemoveFromMap: function (oldMap) {
+            if (this._$control) {
+                this._$control.remove();
+            }
+            Panel.superclass.onRemoveFromMap.call(this, oldMap);
+        },
+
+        _onGetChildElement: function (parentDomContainer) {
+            // Создаем HTML-элемент с текстом.
+            // По-умолчанию HTML-элемент скрыт.
+            this._$control = $('<div class="customControl"><div class="content"></div><div class="closeButton"></div></div>').appendTo(parentDomContainer);
+            this._$content = $('.content');
+            // При клике по крестику будем скрывать панель.
+            $('.closeButton').on('click', this._onClose);
+        },
+        _onClose: function () {
+            $('.customControl').css('display', 'none');
+        },
+        // Метод задания контента панели.
+        setContent: function (text) {
+            // При задании контента будем показывать панель.
+            this._$control.css('display', 'flex');
+            this._$content.html(text);
+        }
+    });
+
+    provide(Panel);
+});
