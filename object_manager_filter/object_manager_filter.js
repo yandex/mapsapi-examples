42ed5224bbcf30e9223474ee3c98a52616fc3e76
sergeiiak <sergeiiak@yandex-team.ru>
2019-04-22 11:42:20 +0100

    14241 : fix jsfiddle export

--- object_manager_filter/object_manager_filter.js
+++ object_manager_filter/object_manager_filter.js
@@ -20,16 +20,20 @@ function init() {
 
     // Creating a drop-down list with 5 items.
     var listBoxItems = ['School', 'Pharmacy', 'Store', 'Hospital', 'Bar']
-        .map(function(title) {
-            return new ymaps.control.ListBoxItem({
-                data: {
-                    content: title
-                },
-                state: {
-                    selected: true
-                }
-            })
-        }),
+            .map(function (title) {
+                return new ymaps.control.ListBoxItem({
+                    data: {
+                        content: title
+                    },
+                    state: {
+                        selected: true
+                    }
+                })
+            }),
+        reducer = function (filters, filter) {
+            filters[filter.data.get('content')] = filter.isSelected();
+            return filters;
+        },
         // Now creating the drop-down list with 5 items.
         listBoxControl = new ymaps.control.ListBox({
             data: {
@@ -40,16 +44,13 @@ function init() {
             state: {
                 // Indicates that the list is expanded.
                 expanded: true,
-                filters: listBoxItems.reduce(function(filters, filter) {
-                    filters[filter.data.get('content')] = filter.isSelected();
-                    return filters;
-                }, {})
+                filters: listBoxItems.reduce(reducer, {})
             }
         });
     myMap.controls.add(listBoxControl);
 
     // Adding tracking to the indicator to check if a list item is selected.
-    listBoxControl.events.add(['select', 'deselect'], function(e) {
+    listBoxControl.events.add(['select', 'deselect'], function (e) {
         var listBoxItem = e.get('target');
         var filters = ymaps.util.extend({}, listBoxControl.state.get('filters'));
         filters[listBoxItem.data.get('content')] = listBoxItem.isSelected();
@@ -57,13 +58,13 @@ function init() {
     });
 
     var filterMonitor = new ymaps.Monitor(listBoxControl.state);
-    filterMonitor.add('filters', function(filters) {
+    filterMonitor.add('filters', function (filters) {
         // Applying the filter.
         objectManager.setFilter(getFilterFunction(filters));
     });
 
-    function getFilterFunction(categories){
-        return function(obj){
+    function getFilterFunction(categories) {
+        return function (obj) {
             var content = obj.properties.balloonContent;
             return categories[content]
         }
